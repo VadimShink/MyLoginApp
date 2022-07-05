23b3a9f1a6493e5f9a6de5f79c09edfc09338c9f
Rigel <vadim_shink@icloud.com>
2022-07-05 13:01:48 +0300

    Initial Commit

---
+++ MyLoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLoginApp
+//
+//  Created by Vadim Shinkarenko on 05.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
